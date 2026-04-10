e2b49b0e3c3e439f09468287465bdab0a6df50b8
Muhammad Kamal Pasha-01 <154614121+pasha-01@users.noreply.github.com>
2026-04-10 13:45:58 +0500

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public uint ReverseBits(uint n) {
+        uint result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;        
+            result |= (n & 1);   
+            n >>= 1;            
+        }
+
+        return result;
+    }
+}
